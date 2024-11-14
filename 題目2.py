d5a350bc3c903acf0b6d94ce05ae122dd362d2d0
BrianTsai071 <91450829+BrianTsai071@users.noreply.github.com>
2024-11-14 08:19:44 +0000

    新增題目二

---
+++ 題目2.py
@@ -0,0 +1,6 @@
+# 正確的變數命名
+number1 = 10
+_name = "Alice"
+total_amount = 100
+
+print(number1, _name, total_amount)
---
+++ 題目2錯誤.py
@@ -0,0 +1,6 @@
+# 錯誤的變數命名
+1number = 10
+_name = "Alice"
+total-amount = 100
+
+print(number1, _name, total_amount)
